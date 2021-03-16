1bb35233edff0123269fc947227c820af0c11ad0
Sergey Lipatnikov <sv.lipatnikov@gmail.com>
2021-03-16 10:43:20 +0000

    Добавлены функции редактирования, удаления, создания нового заказчика

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,19 @@
-/* eslint-disable camelcase */
 /* eslint-disable no-console */
 /* eslint-disable prefer-template */
-const base_url = 'http://3.20.227.226:8000/api/v1';
-
-const token = () => localStorage.getItem('access_token');
+// const apiUrl = 'http://3.20.227.226:8000/api/v1';
+const apiUrl = 'http://build-calculator-dev.spring-intensive-2021.simbirsoft1.com:8000/api/v1';
+const loginUrl = '/login';
 
 export default function sendRequest(url, method, body) {
-  return fetch(base_url + url, {
+  const token = localStorage.getItem('access_token');
+  if (!token) window.location.replace(loginUrl);
+
+  return fetch(apiUrl + url, {
     method,
     body: JSON.stringify(body),
     headers: {
       'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + token(),
+      Authorization: `Bearer ${token}`,
     },
   })
     .then((response) => {
--- src/pages/CustomersListPage/CustomersListPage.js
+++ src/pages/CustomersListPage/CustomersListPage.js
@@ -1,23 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable camelcase */
-import React, { useEffect } from 'react';
-import { Container, Typography, Grid } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Container, Typography, Grid, Button, Box } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
-import customersListSelector from 'redux/selectors/customersListSelector';
+import { customersListSelector } from 'redux/selectors';
 import setCustomersListAction from 'redux/actions/customersListAction';
 import sendRequest from 'api';
+import { snakeToCamelArr } from 'help';
 import CustomerInfo from 'components/CustomerInfo';
 import CustomerCard from './CustomerCard';
 
 const CustomersListPage = () => {
+  const [openModal, setOpenModal] = useState(false);
   const dispatch = useDispatch();
   const storeCustomers = useSelector(customersListSelector);
 
   useEffect(() => {
-    sendRequest('/customers', 'GET').then((data) => {
-      if (data) dispatch(setCustomersListAction(data));
+    sendRequest('/customers/', 'GET').then((data) => {
+      if (data) dispatch(setCustomersListAction(snakeToCamelArr(data)));
     });
   }, []);
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
 
   return (
     <Container maxWidth="lg">
@@ -25,17 +30,23 @@ const CustomersListPage = () => {
         Клиенты
       </Typography>
 
-      <CustomerInfo />
+      <Box my={5}>
+        <Button variant="contained" color="primary" onClick={handleOpenModal}>
+          Создать клиента
+        </Button>
+      </Box>
+
+      <CustomerInfo open={openModal} setOpen={setOpenModal} />
 
       {storeCustomers && (
         <Grid container spacing={3}>
-          {storeCustomers.map(({ id, last_name, first_name, second_name, phone, email, adress }) => (
+          {storeCustomers.map(({ id, lastName, firstName, secondName, phone, email, adress }) => (
             <Grid key={id} item xs={12} sm={6} md={4} lg={3}>
               <CustomerCard
                 id={id}
-                lastName={last_name}
-                firstName={first_name}
-                secondName={second_name}
+                lastName={lastName}
+                firstName={firstName}
+                secondName={secondName}
                 phone={phone}
                 email={email}
                 adress={adress}
