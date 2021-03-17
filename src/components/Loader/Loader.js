e4037604b2790fe3a48f3926f3a544473645dff8
Sergey Lipatnikov <sv.lipatnikov@gmail.com>
2021-03-17 10:14:16 +0000

    Добавлен лоадер

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,24 @@
+/* eslint-disable react/self-closing-comp */
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => (
+  <div className="loader">
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+);
+
+export default Loader;
