da01f047b3b04b6908871ec5a9ee3a6fb6b68020
Lipatnikov <sv.lipatnikov@gmail.com>
2021-03-25 16:38:50 +0400

    fix eng language & add propTypes to customer card

--- src/pages/CustomersListPage/CustomerCard/CustomerCard.js
+++ src/pages/CustomersListPage/CustomerCard/CustomerCard.js
@@ -1,9 +1,9 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { Typography, CardContent, Card, CardActionArea } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { setCurrentCustomerId } from 'redux/actions/currentCustomerAction';
+import PropTypes from 'prop-types';
 
 const CustomerCard = ({ id, lastName, firstName, secondName, phone, email, adress }) => {
   const history = useHistory();
@@ -23,13 +23,29 @@ const CustomerCard = ({ id, lastName, firstName, secondName, phone, email, adres
           <Typography variant="h5" component="h2" color="textPrimary" gutterBottom>
             {`${lastName} ${firstName} ${secondName}`}
           </Typography>
-          <Typography color="textSecondary">{`Phone: ${phone}`}</Typography>
+          <Typography color="textSecondary">{`Телефон: ${phone}`}</Typography>
           <Typography color="textSecondary">{`E-mail: ${email}`}</Typography>
-          <Typography color="textSecondary">{`Adress: ${adress}`}</Typography>
+          <Typography color="textSecondary">{`Адрес: ${adress}`}</Typography>
         </CardContent>
       </CardActionArea>
     </Card>
   );
+};
+
+CustomerCard.propTypes = {
+  id: PropTypes.number.isRequired,
+  lastName: PropTypes.string.isRequired,
+  firstName: PropTypes.string.isRequired,
+  secondName: PropTypes.string,
+  phone: PropTypes.string.isRequired,
+  email: PropTypes.string,
+  adress: PropTypes.string,
+};
+
+CustomerCard.defaultProps = {
+  secondName: '',
+  email: '',
+  adress: '',
 };
 
 export default CustomerCard;
